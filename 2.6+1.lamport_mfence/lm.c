a652384ad8471e79cd23b61fc1821c7d05c9531d
KristinaVarankina <kristjusha@mail.ru>
2012-06-27 14:00:21 +0600

    lampored fixes

--- 2.6+1.lamport_mfence/lm.c
+++ 2.6+1.lamport_mfence/lm.c
@@ -1,83 +1,74 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <pthread.h>
-
 #define mfence asm volatile ("mfence" : : : "memory")
 
 volatile int *Number, *Entering;
 int n;
 int k=0;
-int count=0;
+volatile int res = 0 ;
 
 int max (volatile int* arr ) {
-    int i, max = 0;
-    for ( i = 0; i < n ; i++ ){
-        if ( Number[ i ] > max )
-			{max = Number[ i ];}
-			mfence;}
-	return Number[ i ];
+int i, max = arr[ 0 ];
+for ( i = 1 ; i < n ; i++ )
+if ( Number[ i ] > max )
+return Number[ i ];
+return Number[ 0 ];
 }
-static void* T1 ( void* args ) {
-    pthread_detach( pthread_self() ); 
-    int i = k, j;
-    while ( 1 ) {
-        Entering[ i ] = 1;
-			mfence;
-        Number[ i ] = 1 + max( Number );
-			Entering[ i ] = 0;
-		mfence;
-        for ( j = 0 ; j < n ; j++ ) {
-            while ( Entering[ j ] ) {}
-            while ( Number[ j ] != 0 && ( Number[ j ] <  Number[ i ] || ( Number[ j ] == Number[ i ] && j < i ) ) ) {}
-        }
-		 /* Critical section. */
-        printf( "Thread ID: %d start\n", i );
-        for ( j = 0 ; j < 0xFFFFFF ; j++ ) {mfence;}
-        printf( "Thread ID: %d end\n", i );
-		 /* End of critical section. */
-        Number[ i ] = 0;
-			mfence;
-    }
-    return NULL;
+static void* T1 ( void* args ) 
+{
+pthread_detach( pthread_self() ); 
+int i = (intptr_t)args, j;
+for(int z=0;z<10000000;z++) 
+{
+Entering[ i ] = 1;
+mfence;
+Number[ i ] = 1 + max( Number );
+Entering[ i ] = 0;
+mfence;
+for ( j = 0 ; j < n ; j++ ) {
+while ( Entering[ j ] ) {}
+while ( Number[ j ] != 0 && ( Number[ j ] < Number[ i ] || ( Number[ j ] == Number[ i ] && j < i ) ) ) {}
 }
-//static void* T2 ( void* args ) {
-//    pthread_detach( pthread_self() ); 
- //   int i = 1, j;
- //   while ( 1 ) {
- //       Entering[ i ] = 1;
- //       Number[ i ] = 1 + max( Number );
- //       Entering[ i ] = 0;
-  //      for ( j = 0 ; j < n ; j++ ) {
-  //          while ( Entering[ j ] ) {}
-  //          while ( Number[ j ] != 0 && ( Number[ j ] <  Number[ i ] || ( Number[ j ] == Number[ i ] && j < i ) ) ) {}
-   //     }
-   //     printf( "Thread ID: %d start\n", i );
-    //    for ( j = 0 ; j < 0xFFFFFF ; j++ ) {}
-    //    printf( "Thread ID: %d end\n", i );
-    //    Number[ i ] = 0;
-   // }
-   // return NULL;
-//}		
+printf( "Thread ID: %d start\n", i );
+
+res++;
+mfence;
+printf( "Thread ID: %d end\n", i );
+fprintf (stdout, "%s %d\n","resource=", res);
+Number[ i ] = 0;
+mfence;
+}
+return NULL;
+}
+
 int main (int argc,char **argv) {
-	if(argc != 2)
-        puts("please enter number of processes");
-	n=atoi(argv[1]);
-    Number = (int *)malloc(n*sizeof(int));
-    Entering = (int *)malloc(n*sizeof(int));	
-    int i = 0;
-    pthread_t t1;
+if(argc != 2)
+{
+puts("please enter number of processes");
+exit( -1 );
+}
+n=atoi(argv[1]);
+Number = (int *)malloc(n*sizeof(int));
+Entering = (int *)malloc(n*sizeof(int)); 
+int i = 0;
+pthread_t *threads = (pthread_t *)malloc(n*sizeof(pthread_t));
 
-    for ( i = 0 ; i < n ; i++ )
-        Number[ i ] = Entering[ i ] = 0;	
-	while (count<n){
-   if ( pthread_create( &t1, NULL, T1, NULL ) )
-     exit( -1 );
-	 k+=1;
-	 mfence;
-	 count++;}
- // if ( pthread_create( &t1, NULL, T2, NULL ) )
- //    exit( -1 );
+for ( i = 0 ; i < n ; i++ )
+Number[ i ] = Entering[ i ] = 0; 
+for (k=0;k<n;k++)
+{
+if ( pthread_create( &threads[k], NULL, T1, (void*)(k)) )
+exit( -1 ); 
+}
 
-    while ( 1 ) {}
-    return EXIT_SUCCESS;
+for(int j=n-1; j>=0 ; j--)
+{
+if (pthread_join(threads[j], NULL))
+exit( -1 );
+}
+
+
+return EXIT_SUCCESS;
 }
